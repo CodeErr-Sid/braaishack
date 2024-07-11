8d6abb40a4439e2de5a6ad29b6d12b26d49691a5
CodeErr-Sid <siddiqueofl@gmail.com>
2024-07-11 19:58:09 +0530

    working file

--- frontend/src/components/ResturentOption/ResturentOption.jsx
+++ frontend/src/components/ResturentOption/ResturentOption.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './ResturentOption.css';
+import {assets} from "../../assets/assets";
 
 const ResturentOption = () => {
   return (
     <>
-    <div className="mian"></div>
+    <div className="main">
+      <h1 className='mainhead'>What do you like Today ?</h1>
+      <div className="menulists">
+        <div className="menulist">
+          <div className="menuimg">
+            <img src={assets.steek} alt="" />
+          </div>
+          <div className="menucontent">
+            <h1>hello</h1>
+            <p className='para'>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
