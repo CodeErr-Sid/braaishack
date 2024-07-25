a77c7f64132c070e96223903b4097c09240082b1
CodeErr-Sid <siddiqueofl@gmail.com>
2024-07-25 10:08:15 +0530

    cards img added  json file solve problem

--- frontend/src/components/Menucards/Menucards.jsx
+++ frontend/src/components/Menucards/Menucards.jsx
@@ -1,169 +1,13 @@
 import React, { useEffect, useRef } from "react";
-import { useNavigate } from "react-router-dom";
 import "./Menucard.css";
+import { useNavigate } from "react-router-dom";
+import { sections } from "../../Cardsdata";
 import { assets } from "../../assets/assets";
+
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
-
-const sections = [
-  {
-    title: "Starters",
-    cards: [
-      {
-        img: assets.starters1,
-        productname: "samosa",
-        productprice: "$49",
-      },
-      {
-        img: assets.starters2,
-        productname: "pakora",
-        productprice: "$39",
-      },
-      {
-        img: assets.starters3,
-        productname: "spring roll",
-        productprice: "$29",
-      },
-      {
-        img: assets.starters4,
-        productname: "chicken wings",
-        productprice: "$59",
-      },
-    ],
-  },
-  {
-    title: "Braai Menu",
-    cards: [
-      {
-        img: assets.braai1,
-        productname: "samosa",
-        productprice: "$49",
-      },
-      {
-        img: assets.braai2,
-        productname: "pakora",
-        productprice: "$39",
-      },
-      {
-        img: assets.braai3,
-        productname: "spring roll",
-        productprice: "$29",
-      },
-      {
-        img: assets.braai4,
-        productname: "chicken wings",
-        productprice: "$59",
-      },
-      {
-        img: assets.braai5,
-        productname: "chicken wings",
-        productprice: "$59",
-      },
-      {
-        img: assets.braai6,
-        productname: "chicken wings",
-        productprice: "$59",
-      },
-      {
-        img: assets.braai7,
-        productname: "chicken wings",
-        productprice: "$59",
-      },
-    ],
-  },
-  {
-    title: "Kids Braai Menu",
-    cards: [
-      {
-        img: assets.kids1,
-        productname: "samosa",
-        productprice: "$49",
-      },
-      {
-        img: assets.kids2,
-        productname: "pakora",
-        productprice: "$39",
-      },
-      {
-        img: assets.kids3,
-        productname: "spring roll",
-        productprice: "$29",
-      },
-    ],
-  },
-  {
-    title: "Braai Sides",
-    cards: [
-      {
-        img: assets.sides1,
-        productname: "samosa",
-        productprice: "$49",
-      },
-      {
-        img: assets.sides2,
-        productname: "pakora",
-        productprice: "$39",
-      },
-      {
-        img: assets.sides3,
-        productname: "spring roll",
-        productprice: "$29",
-      },
-      {
-        img: assets.sides4,
-        productname: "chicken wings",
-        productprice: "$59",
-      },
-      {
-        img: assets.sides5,
-        productname: "chicken wings",
-        productprice: "$59",
-      },
-      {
-        img: assets.sides6,
-        productname: "chicken wings",
-        productprice: "$59",
-      },
-      {
-        img: assets.sides7,
-        productname: "chicken wings",
-        productprice: "$59",
-      },
-      {
-        img: assets.sides8,
-        productname: "chicken wings",
-        productprice: "$59",
-      },
-    ],
-  },
-  {
-    title: "Desserts",
-    cards: [
-      {
-        img: assets.sides9,
-        productname: "samosa",
-        productprice: "$49",
-      },
-      {
-        img: assets.sides10,
-        productname: "pakora",
-        productprice: "$39",
-      },
-      {
-        img: assets.sides11,
-        productname: "spring roll",
-        productprice: "$29",
-      },
-      {
-        img: assets.sides12,
-        productname: "chicken wings",
-        productprice: "$59",
-      },
-    ],
-  },
-];
 
 const Menucards = ({ selectedMenu }) => {
   const cardRefs = useRef([]);
@@ -217,13 +61,23 @@ const Menucards = ({ selectedMenu }) => {
           className="cardmain"
           id={section.title.replace(/\s+/g, "")}
           key={sectionIndex}
-          style={{ display: selectedMenu === section.title || selectedMenu === "" ? "block" : "none" }} >
-          <h1 ref={(el) => (headingRefs.current[sectionIndex] = el)}>{section.title}</h1>
+          style={{
+            display:
+              selectedMenu === section.title || selectedMenu === "" ? "block" : "none",
+          }}
+        >
+          <h1 ref={(el) => (headingRefs.current[sectionIndex] = el)}>
+            {section.title}
+          </h1>
           <div className="cards">
             {section.cards.map((card, cardIndex) => (
-              <div className="card" ref={(el) => (cardRefs.current[cardIndex + sectionIndex * 10] = el)} key={cardIndex}>
+              <div
+                className="card"
+                ref={(el) => (cardRefs.current[cardIndex + sectionIndex * 10] = el)}
+                key={cardIndex}
+              >
                 <div className="cardimg">
-                  <img src={card.img} alt={card.productname} />
+                  <img src={assets[card.img]} alt={card.productname} />
                 </div>
                 <div className="cardcontent">
                   <div className="left-card-content">
