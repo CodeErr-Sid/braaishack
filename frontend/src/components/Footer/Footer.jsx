eb1f1a736d19670018ceeca4f7e1e723a5d622e3
Rasmi142 <rasmioff@gmail.com>
2024-07-25 14:17:48 +0530

    footer modified

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -6,31 +6,22 @@ const Footer = () => {
   return (
     <div className='footer' id='footer'>
       <div className="footer-content">
+
         <div className="footer-content-left">
             <img src={assets.logo} alt="" className='logo'/>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-            <div className="footer-social-icons">
-                <img src={assets.facebook_icon} alt="" />
-                <img src={assets.twitter_icon} alt="" />
-                <img src={assets.linkedin_icon} alt="" />
-            </div>
+             <p className='address'>42 Church Street, Wolverton MK 12 5JN Milton Keynes</p>
+             <p className='phone-number'>01908 313838</p>
         </div>
-        <div className="footer-content-center">
-            <h2>COMPANY</h2>
-            <ul>
-                <li>Home</li>
-                <li>About us</li>
-                <li>Delivery</li>
-                <li>Privacy policy</li>
-            </ul>
-        </div>
+  
         <div className="footer-content-right">
-            <h2>GET IN TOUCH</h2>
-            <ul>
-                <li>+1-212-456-7890</li>
-                <li>contact@tomato.com</li>
-            </ul>
+            <h2>Newsletter</h2>
+             <p>Lorem ipsum is simply dummy text of the printing and typesetting industry.</p>
+             <div className='subscribe'>
+              <input type='email' placeholder='Your Email Address' required/>
+              <button>Subscribe</button>
+             </div>
         </div>
+
       </div>
       <hr />
       <div className="babyfoot">
