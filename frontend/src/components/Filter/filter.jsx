afb4a5f4d580307ea7aecd6dc55766f3d9c97cf3
primesiteinnovation <primesiteinnovation@gmail.com>
2024-07-29 13:22:54 +0530

    Production Commit - Siddique

--- frontend/src/components/Filter/filter.jsx
+++ frontend/src/components/Filter/filter.jsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilter } from "@fortawesome/free-solid-svg-icons";
 import "./filter.css";
 
+
 const Filter = ({ setSelectedMenu }) => {
   const [showMenu, setShowMenu] = useState(false);
   const Menubtndata = [
