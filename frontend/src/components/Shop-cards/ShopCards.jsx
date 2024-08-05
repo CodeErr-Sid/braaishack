8b8fa1c937a253a477e38417d6776c41bde34695
Ali Akram <aliakram9789@gmail.com>
2024-08-05 18:14:53 +0530

    some minor correction on designs

--- frontend/src/components/Shop-cards/ShopCards.jsx
+++ frontend/src/components/Shop-cards/ShopCards.jsx
@@ -25,57 +25,62 @@ export default function ShopCards({ shopCardData, selectedMenu }) {
             </div>
             <div className="container">
                 <div className="row">
-                    {shopCardData.map((item) => (
-                        <div
-                            id={`item-${item.id}`} // Ensure the id is unique
-                            key={item.id} // Prefer unique id over index
-                            style={{
-                                display:
-                                    selectedMenu === item.category || selectedMenu === "" || selectedMenu === "All" ? "flex" : "none",
-                            }}
-                            className="col-lg-4"
-                        >
-                            <div className="sb-menu-item sb-mb-30">
-                                <a href="product.html" className="sb-cover-frame">
-                                    <img src={assets[item.image]} alt={item.name} className='cards-img' />
-                                </a>
-                                <div className="sb-card-tp">
-                                    <h4 className="sb-card-title">
-                                        <a href="product.html">{item.name}</a>
-                                    </h4>
-                                    <div className="sb-price">
-                                        <sub>$</sub> {item.price}
+                    {shopCardData.map((item) => {
+                        const shouldDisplay = selectedMenu === item.category || selectedMenu === "" || selectedMenu === "All";
+                        return (
+                            <div
+                                id={`item-${item.id}`} // Ensure the id is unique
+                                key={item.id} // Prefer unique id over index
+
+                                style={{
+                                    opacity: shouldDisplay ? 1 : 0,
+                                    transition: 'opacity 0.5s ease-in-out',
+                                    display: shouldDisplay ? 'flex' : 'none',
+                                }}
+                                className="col"
+                            >
+                                <div className="sb-menu-item sb-mb-30">
+                                    <a href="#" className="sb-cover-frame">
+                                        <img src={assets[item.image]} alt={item.name} className='cards-img' />
+                                    </a>
+                                    <div className="sb-card-tp">
+                                        <h4 className="sb-card-title">
+                                            <a href="#">{item.name}</a>
+                                        </h4>
+                                        <div className="sb-price">
+                                            <sub>$</sub> {item.price}
+                                        </div>
+                                    </div>
+                                    <div className="sb-description">
+                                        <p className="sb-text sb-mb-15">{item.description}</p>
+                                    </div>
+                                    <div className="sb-card-buttons-frame">
+                                        <Link to={`/product`} className="sb-btn sb-btn-2 sb-btn-gray sb-btn-icon sb-m-0">
+                                            <span className="sb-icon">
+                                                <img src={assets.arrow2} alt="icon" />
+                                            </span>
+                                            <span>Details</span>
+                                        </Link>
+                                        <a
+                                            href="#."
+                                            className="sb-btn sb-atc"
+                                            onClick={() => handleAddToCart(item)}
+                                        >
+                                            <span className="sb-icon">
+                                                <FontAwesomeIcon icon={faCartShopping} style={{ color: "black" }} />
+                                            </span>
+                                            <span className="sb-add-to-cart-text">
+                                                {addedItems[item.id] ? 'Added' : 'Add to cart'}
+                                            </span>
+                                            {addedItems[item.id] && (
+                                                <span className="sb-added-text"></span>
+                                            )}
+                                        </a>
                                     </div>
                                 </div>
-                                <div className="sb-description">
-                                    <p className="sb-text sb-mb-15">{item.description}</p>
-                                </div>
-                                <div className="sb-card-buttons-frame">
-                                    <Link to={`/product`} className="sb-btn sb-btn-2 sb-btn-gray sb-btn-icon sb-m-0">
-                                        <span className="sb-icon">
-                                            <img src={assets.arrow2} alt="icon" />
-                                        </span>
-                                        <span>Details</span>
-                                    </Link>
-                                    <a
-                                        href="#."
-                                        className="sb-btn sb-atc"
-                                        onClick={() => handleAddToCart(item)}
-                                    >
-                                        <span className="sb-icon">
-                                            <FontAwesomeIcon icon={faCartShopping} style={{ color: "black" }} />
-                                        </span>
-                                        <span className="sb-add-to-cart-text">
-                                            {addedItems[item.id] ? 'Added' : 'Add to cart'}
-                                        </span>
-                                        {addedItems[item.id] && (
-                                            <span className="sb-added-text"></span>
-                                        )}
-                                    </a>
-                                </div>
                             </div>
-                        </div>
-                    ))}
+                        )
+                    })}
                 </div>
             </div>
         </section>
