04665fcdba4d606ee4f0601666fa19c9b4414843
unknown <siddiqueofl@gmail.com>
2024-08-25 19:48:01 +0530

    reservation

--- frontend/src/components/Zenchef/Zenchef.jsx
+++ frontend/src/components/Zenchef/Zenchef.jsx
@@ -3,12 +3,14 @@ import './Zenchef.css';
 
 const ZenchefBooking = () => {
   return (
+    <div className="zenchef_body">
     <div className="widget-container">
       <iframe
         src="https://bookings.zenchef.com/results?rid=344015&showCollapsed=1"
         allow="payment"
         title="Zenchef Booking"
       ></iframe>
+    </div>
     </div>
   );
 };
