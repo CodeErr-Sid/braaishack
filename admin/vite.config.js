cc411943c08ba7233c5dc85266050805dd8398a1
Ali Akram <aliakram9789@gmail.com>
2024-08-27 20:25:30 +0530

    vite config is updated

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5174 },
-})
+  server: { 
+    port: 5174 
+  },
+  // No need to define process.env, Vite handles this automatically
+  define: {
+    'process.env': {}  // Optional polyfill if needed for some legacy libraries
+  },
+});
