995fd039ae260b8338ee8d8c65fa3d48067c016e
Akram-00 <aliakram9789@gmail.com>
2024-09-19 20:25:43 +0530

    steps to follow added

---
+++ frontend/src/components/Steps-to-follow/StepsToFollow.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import styles from './StepsToFollow.module.css'
+
+const StepsToFollow = () => {
+
+    const steps = [
+        "Lorem ipsum dolor sit",
+        "Lorem ipsum dolor sit",
+        "Lorem ipsum dolor sit"
+    ]
+
+    return (
+        <section className={styles.stepstofollowcontainer}>
+            <h1 className={styles.stepheader}>Steps to Follow:</h1>
+            <p className={styles.stepsdescription}>
+            Lorem ipsum dosectetur adipisicing elit, sed do.Lorem ipsum dolor sit amet, consectetur Nulla fringilla  Lorem ipsum dosectetur adipisicing elit, sed do.Lorem ipsum dolor sit amet, consectetur Nulla fringilla purus at leo dignissim congue. Lorem ipsum dosectetur adipisicing elit, sed do.Lorem ipsum dolor sit amet, consectetur Nulla fringilla purus at leo dignissim congue. 
+            Lorem ipsum dosectetur adipisicing elit, sed do.Lorem ipsum dolor sit amet, consectetur Nulla fringilla purus at leo dignissim congue. Lorem ipsum dosectetur adipisicing elit, sed do.Lorem ipsum dolor sit amet, consectetur Nulla fringilla purus at leo dignissim congue. 
+            </p>
+            <div className={styles.steps}>
+                {steps?.map((step, index) => (
+                    <div className={styles.stepcontainer} key={index}>
+                        <div className={styles.stepnumbercircle}>
+                            <div className={styles.stepnumber}>
+                                {index + 1}
+                            </div>
+                        </div>
+                        <div className={styles.stepdescription}>
+                            {step}
+                        </div>
+
+                    </div>
+                ))}
+            </div>
+        </section>
+    )
+}
+
+export default StepsToFollow
--- frontend/src/pages/GiftSection/GiftSection.jsx
+++ frontend/src/pages/GiftSection/GiftSection.jsx
@@ -2,16 +2,15 @@ import React from 'react'
 import Header from '../../components/About-header/Header'
 import Gift_Coupen from '../../components/Gift_coupen/Gift_Coupen'
 import Preloader from '../../components/Preloader/Preloader';
+import StepsToFollow from '../../components/Steps-to-follow/StepsToFollow';
 
 const GiftSection = () => {
   return (
     <>
-        <Preloader />
-        <Header header_title={'GIFT SECTION'} header_btn={'HOME/GIFT'} />
-        
-        <Gift_Coupen/>
-        <Gift_Coupen/>
-        <Gift_Coupen/>
+      <Preloader />
+      <Header header_title={'GIFT SECTION'} header_btn={'HOME/GIFT'} />
+      <StepsToFollow />
+      <Gift_Coupen />
     </>
   )
 }
